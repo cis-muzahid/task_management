c8acafb0244925922f6b0de8fb470771c4d214bc
anurag-cis <144808773+anurag-cis@users.noreply.github.com>
2024-07-16 21:53:11 +0530

    Delete task_management_backend directory

--- task_management_backend/tasks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import TaskListCreateView, TaskDetailView, MultipleTaskUpdateView, TaskStartStopView
-
-urlpatterns = [
-    path('', TaskListCreateView.as_view(), name='task-list-create'),
-    path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-    path('batch-update/', MultipleTaskUpdateView.as_view(), name='task-batch-update'),
-    path('start-stop/', TaskStartStopView.as_view(), name='task-start-stop'),
-]
