ed9bf4980dfcc4a9427a851a04c5c24ed8b37dd5
anurag-cis <anurag.pr@cisinlabs.com>
2024-07-29 21:51:21 +0530

    Added docker

--- backend/task_management_backend/settings.py
+++ backend/task_management_backend/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DATABASE_NAME'),
+#         'USER': config('DATABASE_USER'),
+#         'PASSWORD': config('DATABASE_PASSWORD'),
+#         'HOST': config('DATABASE_HOST'),
+#         'PORT': config('DATABASE_PORT', 5432),
+#     }
+# }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <Router>
           <Routes>
             <Route
-              path="/a"
+              path="/"
               element={
                 <PrivateRoute>
                   <Dashboard />
